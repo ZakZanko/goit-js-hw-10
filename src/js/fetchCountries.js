70e81c35f63e900cc497417ae4f9406aa1f40db2
ZakZanko <zakzanko2112@gmail.com>
2023-05-18 16:57:07 +0300

    n

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,9 @@
 const fields = 'fields=name,capital,population,flags,languages';
-const BASE_URL = 'https://restcountries.com/v3.1/name';
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-export function fetchCountries(name) {
+function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}?${fields}`)
     .then(response => response.json())
     .catch(error => console.log(error));
 }
+export { fetchCountries };
