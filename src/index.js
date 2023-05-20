79290c4e464f6ed8f5e06709e0c8815ad1c3726f
ZakZanko <zakzanko2112@gmail.com>
2023-05-20 16:15:42 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function renderList(countries) {
     .map(({ name, flags }) => {
       return `
           <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 55px height = 35px>
               <h2 class="country-list__name">${name.official}</h2>
           </li>
           `;
